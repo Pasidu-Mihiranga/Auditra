600feeac6030dd61939184d0c8e721014373ebcb
Pasidu-Mihiranga <ilamperumapm.23@uom.lk>
2025-12-22 21:20:45 +0530

    Strip spaces from email password (Gmail app passwords)

--- backend/auditra_backend/settings.py
+++ backend/auditra_backend/settings.py
@@ -177,7 +177,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+# Gmail app passwords may have spaces - remove them
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='').replace(' ', '')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
 
 # Email timeout settings
